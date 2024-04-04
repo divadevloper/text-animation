571522bd1c625a52101fa00e3d1ba79530c2b738
Danicode <ariyoesther2009@gmail.com>
2024-04-04 11:25:05 +0100

    added some

--- function.js
+++ function.js
@@ -1,21 +1,43 @@
 let text = document.getElementById("text");
 let textword = "I'm a fullstack developer";
+let index = 0
+let forward = true
 
+setInterval(() => {
+    if (index < textword.length && forward) {
+        text.innerHTML += textword[index]
+        index ++
+    }else if( index > 0){
+      forward= false;
+      index -- ;
+  text.innerHTML = textword.substring(0, index)
 
-
-function animatin(element,text, i=0) {
-
-    if (i === 0) {
-        element.innerHTML = '';
     }
-    element.innerHTML += text[i];
-    if (i === textword.length -1) {
-        return;
+    else{
+        forward=true
     }
+}, 50);
 
 
-    setTimeout(() =>  animatin(element,text , i+1), 50);
-   
+// function animatin(element, text, i = 0) {
 
-}
-animatin(text, textword)
+//     if (i === 0) {
+//         element.innerHTML = '';
+//     }
+//     element.innerHTML += text[i];
+//     if (i === textword.length - 1) {
+        
+//     }
+
+
+//     setTimeout(() => animatin(element, text, i + 1), 100);
+
+//     if (i > textword.length) {
+//         textword.substring(0, i)
+//         
+      
+//     }
+
+
+// }
+// animatin(text, textword)
